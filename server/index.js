cbb6fe1f13e9bf5f8bf736cb8e881cde1905f406
ALEKSEI <ponibrashin@yandex.ru>
2022-12-07 05:47:20 +0700

    static

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 // Cross-Origin Resource Sharing
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 // middleware для работы с json
 app.use(express.json());
 // middleware для статики (img, css)
