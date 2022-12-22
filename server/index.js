aedb4cf84f14626e02530b286498d753decce62c
ALEKSEI <ponibrashin@yandex.ru>
2022-12-22 19:04:56 +0700

    add images

--- server/index.js
+++ server/index.js
@@ -14,10 +14,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 // Cross-Origin Resource Sharing
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(cors({ origin: "http://yaq.kz", credentials: true }));
 // middleware для работы с json
 app.use(express.json());
 // middleware для статики (img, css)
-app.use(express.static("static"));
+app.use(express.static("images"));
 // cookie для корзины
 app.use(cookieParser(process.env.SECRET_KEY));
 // middleware для загрузки файлов
